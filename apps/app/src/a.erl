a74e140c853756745209b558026fb673b5430f93
Paul Peregud <paulperegud@gmail.com>
2014-03-18 15:45:35 +0100

    moving calls to meck outside of eunit main process prevents crashes

--- apps/app/src/a.erl
+++ apps/app/src/a.erl
@@ -7,20 +7,43 @@ c() ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(OTHER, b).
+
+-define(DBG(F, A), io:fwrite(user, "~p@~p:~p :: " ++ F ++ "~n", [self(), ?MODULE, ?LINE] ++ A)).
+%% -define(DBG(F, A), debugFmt("~p@~p:~p :: " ++ F ++ "~n", [self(), ?MODULE, ?LINE] ++ A)).
+
+zetests() ->
+    fun(_) ->
+	    %% {spawn,
+ 	     [ ?_assert(true) ]
+            %% }
+    end.
+
+
 a_test_() ->
     {setup,
+     spawn,
      fun() ->
-             meck:new(b,[no_passthrough_cover])
+	     Pid = proc_lib:spawn(fun() ->
+					  meck:new(?OTHER,[no_passthrough_cover]),
+					  receive
+					      stop ->
+						  %% meck:unload(?OTHER),
+						  ok
+					  end
+	     			  end),
+	     register(?MODULE, Pid)
      end,
      fun(_) ->
              %% the following should match otherwise code:purge in meck:unload
              %% will fail (it will kill test process)
-             %% [] = [P || P <- processes(), erlang:check_process_code(P, b), P == self()],
-             meck:unload()
+             R = [P || P <- processes(), erlang:check_process_code(P, ?OTHER), P == self()],
+	     ?DBG("users of old code of module ~p: ~p", [?OTHER, R]),
+	     ?MODULE ! stop,
+	     spawn(fun() -> meck:unload(?OTHER) end)
+             %% meck:unload(?OTHER)
      end,
-     fun(_) ->
-             [ ?_assert(true) ]
-     end
+     zetests()
     }.
 
 -endif.
--- apps/app/src/b.erl
+++ apps/app/src/b.erl
@@ -1,25 +1,45 @@
 -module(b).
 -compile(export_all).
 
-test_fun() ->
-    ok.
+%% test_fun() ->
+%%     ok.
+
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(DBG(F, A), io:fwrite(user, "~p@~p:~p :: " ++ F ++ "~n", [self(), ?MODULE, ?LINE] ++ A)).
+%% -define(DBG(F, A), debugFmt("~p@~p:~p :: " ++ F ++ "~n", [self(), ?MODULE, ?LINE] ++ A)).
+
+-define(OTHER, a).
+
+zetests() ->
+    fun(_) ->
+	    %% {spawn,
+	     [ ?_assert(true) ]
+    %% 	    }
+    end.
+
 b_test_() ->
     {setup,
+     spawn,
      fun() ->
-             meck:new(a,[no_passthrough_cover])
+	     Pid = proc_lib:spawn(fun() ->
+					  meck:new(?OTHER,[no_passthrough_cover]),
+					  receive
+					      stop ->
+						  ok
+					  end
+	     			  end),
+	     register(?MODULE, Pid)
      end,
      fun(_) ->
-%             [] = [P || P <- processes(), erlang:check_process_code(P, a), P == self()],
-%             meck:unload()
-ok
+             R = [P || P <- processes(), erlang:check_process_code(P, ?OTHER), P == self()],
+	     ?DBG("users of old code of module ~p: ~p", [?OTHER, R]),
+	     ?MODULE ! stop,
+	     spawn(fun() -> meck:unload(?OTHER) end)
      end,
-     fun(_) ->
-             [ ?_assert(true) ]
-     end
+     zetests()
     }.
 
 -endif.
